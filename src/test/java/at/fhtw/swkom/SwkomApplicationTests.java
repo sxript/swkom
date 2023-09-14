4b76c4bce64fbbd440124481b0a7d843c9c2e8dc
Marko Pavlovic <markopavlovic205051@gmail.com>
2023-09-14 15:00:16 +0200

    initial commit

---
+++ src/test/java/at/fhtw/swkom/SwkomApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.swkom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwkomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
