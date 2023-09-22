1d9ae174a0881a9ab15c2fdc1ababeadf49be77b
Filip Adamovic <45206898+SXRIPT@users.noreply.github.com>
2023-09-22 20:51:33 +0200

    chore: rename api

--- src/main/java/at/fhtw/swkom/paperless/controller/PaperlessApi.java
+++ src/main/java/at/fhtw/swkom/paperless/controller/PaperlessApi.java
@@ -15,39 +15,30 @@ import at.fhtw.swkom.paperless.services.dto.NewDocumentType;
 import at.fhtw.swkom.paperless.services.dto.NewTag;
 import java.time.OffsetDateTime;
 import at.fhtw.swkom.paperless.services.dto.UserInfo;
-import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import org.springframework.http.codec.multipart.Part;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.*;
+
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+
 import jakarta.annotation.Generated;
 
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
 @Validated
 @Tag(name = "Login", description = "the Login API")
-public interface ApiApi {
+public interface PaperlessApi {
 
     /**
      * GET /api
