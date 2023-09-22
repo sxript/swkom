ba0710fd6f5d0a63494486bdd74fd19555f322ad
Filip Adamovic <45206898+SXRIPT@users.noreply.github.com>
2023-09-22 20:51:33 +0200

    chore: rename api

---
+++ src/main/java/at/fhtw/swkom/paperless/controller/PaperlessApiController.java
@@ -0,0 +1,14 @@
+package at.fhtw.swkom.paperless.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.annotation.Generated;
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen")
+@Controller
+@RequestMapping("${openapi.mockServer.base-path:}")
+public class PaperlessApiController implements PaperlessApi {
+
+}
