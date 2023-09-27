4ca387f3af724038c4ffaf415c17e5e41bad8d93
Marko Pavlovic <markopavlovic205051@gmail.com>
2023-09-27 19:47:11 +0200

    feature: entiities

---
+++ src/main/java/at/fhtw/swkom/paperless/services/persistance/NewDocumentType.java
@@ -0,0 +1,30 @@
+package at.fhtw.swkom.paperless.services.persistance;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "new_correspondent")
+public class NewDocumentType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+
+    private String match;
+
+    @NotNull
+    @Column(name = "matching_algorithm")
+    private int matchingAlgortihm;
+    @NotNull
+    @Column(name = "is_insensitive")
+    private boolean isSensitive;
+
+}
