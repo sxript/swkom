3480ab66d9ed6bde5aa675d469d90da0b3504819
Marko Pavlovic <markopavlovic205051@gmail.com>
2023-11-12 23:05:20 +0100

    refactorin

--- src/main/java/com/paperless/services/impl/DocumentService.java
+++ src/main/java/com/paperless/services/impl/DocumentService.java
@@ -1,5 +1,6 @@
 package com.paperless.services.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.paperless.persistence.entities.Document;
 import com.paperless.services.dto.DocumentDTO;
 import org.springframework.web.multipart.MultipartFile;
@@ -7,7 +8,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 public interface DocumentService {
-    void uploadDocument(DocumentDTO documentDTO, List<MultipartFile> document);
+    void uploadDocument(DocumentDTO documentDTO) throws JsonProcessingException;
 
     List<Document> getDocuments();
 }
