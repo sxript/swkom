1fb76066c1ad2849ba8b95f50bfc7ac5ffa715f1
Marko Pavlovic <markopavlovic205051@gmail.com>
2023-11-12 13:20:32 +0100

    add rabbitMQ and upload

--- src/main/java/com/paperless/api/PaperlessApplication.java
+++ src/main/java/com/paperless/api/PaperlessApplication.java
@@ -1,10 +1,17 @@
 package com.paperless.api;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.paperless.services.dto.DocumentDTO;
 import com.paperless.services.dto.gets.GetDocuments200Response;
 import com.paperless.services.impl.DocumentServiceImpl;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.openapitools.jackson.nullable.JsonNullable;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,13 +23,16 @@ import org.springframework.web.multipart.MultipartFile;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.amqp.core.Queue;
 import javax.annotation.Generated;
 
 @Generated(value = "com.paperless.codegen.languages.SpringCodegen", date = "2023-10-10T06:36:40.060738Z[Etc/UTC]")
 @Controller
 @RequestMapping("${openapi.paperlessRestServer.base-path:}")
 @CrossOrigin(origins = "http://localhost:8080")
+@Slf4j
 public class PaperlessApplication implements PaperlessApi {
+
 
     private final NativeWebRequest request;
 
@@ -30,9 +40,18 @@ public class PaperlessApplication implements PaperlessApi {
     DocumentServiceImpl documentService;
 
     @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    private Queue ocrScanQueue;
+
+    @Autowired
     public PaperlessApplication(NativeWebRequest request) {
         this.request = request;
     }
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Override
     public Optional<NativeWebRequest> getRequest() {
@@ -44,7 +63,8 @@ public class PaperlessApplication implements PaperlessApi {
 
 
       try{
-          String name = document.get(0).getOriginalFilename();
+          MultipartFile documentt = document.get(0);
+          String name = documentt.getOriginalFilename();
           DocumentDTO documentDTO = new DocumentDTO();
 
 
@@ -59,9 +79,17 @@ public class PaperlessApplication implements PaperlessApi {
 
           documentService.uploadDocument(documentDTO,document);
 
+
+          // Convert DocumentDTO to JSON and send to RabbitMQ
+          String documentJson = objectMapper.writeValueAsString(documentDTO);
+          rabbitTemplate.convertAndSend(ocrScanQueue.getName(), documentJson);
+          log.info("Document uploaded and sent to RabbitMQ for OCR processing.");
+
+
           return new ResponseEntity<>(HttpStatus.OK);
 
       }catch (Exception e){
+          log.error(e.getMessage());
           return ResponseEntity.internalServerError().build();
       }
     }
---
+++ src/main/java/com/paperless/configuration/RabbitMQConfig.java
@@ -0,0 +1,15 @@
+package com.paperless.configuration;
+
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public Queue ocrScanQueue() {
+        return new Queue("ocrScanQueue");
+    }
+
+}
